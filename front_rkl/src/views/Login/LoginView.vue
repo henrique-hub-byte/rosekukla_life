a32b28110b1e3a8a725601a4f633525f61808b82
luis henrique martins dos santos bonfim <luis75henrique75@gmail.com>
2023-02-02 22:41:45 -0300

    add initial files

---
+++ front_rkl/src/views/Login/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="main">
+        <div class="login">
+            <div class="content_login">
+
+                <!-- <div class="login_box"> -->
+                <h2>Login</h2>
+                <form>
+                    <div class="user_box">
+
+                        <input id='email' type="text" required >
+                        <label for='email'>E-mail</label>
+                    </div>
+                    <div class="user_box">
+
+                        <input id='senha' type="password" name="" required>
+                        <label for='senha'>Senha</label>
+                    </div>
+                    <a href="#">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        Submit
+                    </a>
+                </form>
+                <!-- </div> -->
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" src="./style.scss" scoped>
+
+</style>
